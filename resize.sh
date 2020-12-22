0a6c5be84bbe6d0fa077fc43d653c25905080aaa
Tony Malykh <anton.malykh@gmail.com>
2020-12-22 14:51:14 -0800

    Adding icons

---
+++ resize.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+convert blindbat.png -resize 58x58 blindbat58.png
+convert blindbat.png -resize 80x80 blindbat80.png
+convert blindbat.png -resize 120x120 blindbat120.png
+convert blindbat.png -resize 180x180 blindbat180.png
